b174f72b3649717b1e40c2832a253837f54b6d65
saketh523 <jammulasakethreddy@gmail.com>
2020-04-01 22:33:05 -0700

    Add files via upload

---
+++ EmojiConvertor.py
@@ -0,0 +1,13 @@
+#EmojiConvertor
+
+message = input(">")
+words = message.split(' ')
+emojis = {
+   ":)" : "😄",
+   ":(" : "😞"
+
+}
+output = ""
+for word in words:
+  output += emojis.get(word,word) + ' '
+print(output)
