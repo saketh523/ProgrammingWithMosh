d61faec9ecc07dd600c95c65d9a058af427b1b9d
saketh523 <jammulasakethreddy@gmail.com>
2020-04-05 02:01:23 -0700

    Update MusicSuggestion.py
    My first machine learning program using music.csv as sample dataset to predict the music interest of an individual based on age and gender.

--- MusicSuggestion.py
+++ MusicSuggestion.py
@@ -1,12 +1,10 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 
 music_data = pd.read_csv('music.csv')
-music_data
 X = music_data.drop(columns = ['genre'])
 y = music_data['genre']
+
 model = DecisionTreeClassifier()
 model.fit(X,y)
 predictions = model.predict([[22,0]])
